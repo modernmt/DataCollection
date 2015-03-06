687afc973921f4bce5e58db225b5c2651c04aebd
Christian Buck <cbuck@lantis.de>
2015-03-06 15:12:10 +0000

    added gumbo based html2text util

---
+++ html_convert/html2text.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <set>
+
+#include "gumbo.h"
+
+using std::string;
+using std::getline;
+using std::cin;
+using std::cout;
+
+// Tags that define a block and thus introduce a line break
+static std::set<string> block_tags = {
+    "address", "article", "aside",  "audio",    "blockquote", "canvas",
+    "dd",      "div",     "dl",     "fieldset", "figcaption", "figure",
+    "footer",  "form",    "h1",     "h2",       "h3",         "h4",
+    "h5",      "h6",      "header", "hgroup",   "hr",         "noscript",
+    "ol",      "output",  "p",      "pre",      "section",    "table",
+    "tfoot",   "ul",      "video"};
+
+static void DumpText(GumboNode* node, std::ostringstream& oss) {
+  if (node->type == GUMBO_NODE_TEXT) {
+    string text(node->v.text.text);
+    std::replace(text.begin(), text.end(), '\n', ' ');
+    oss << text;
+  } else if (node->type == GUMBO_NODE_WHITESPACE) {
+    oss << " ";
+  } else if (node->type == GUMBO_NODE_ELEMENT &&
+             node->v.element.tag != GUMBO_TAG_SCRIPT &&
+             node->v.element.tag != GUMBO_TAG_STYLE) {
+    // Insert line breaks for <br> and <li>
+    if (node->v.element.tag == GUMBO_TAG_BR ||
+        node->v.element.tag == GUMBO_TAG_LI) {
+      oss << std::endl;
+    }
+    // Descend into subtree
+    GumboVector* children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; ++i) {
+      DumpText((GumboNode*)children->data[i], oss);
+    }
+
+    const std::string tagname = gumbo_normalized_tagname(node->v.element.tag);
+    // Insert line break if tag defines a block
+    if (block_tags.find(tagname) != block_tags.end()) {
+      oss << std::endl;
+    }
+  }
+}
+
+void ProcessBuffer(const string& header, const string& buffer) {
+  if (header.empty() || buffer.empty()) {
+    return;
+  }
+  GumboOutput* output = gumbo_parse(buffer.c_str());
+  std::cout << header << std::endl;
+  std::ostringstream extracted_text;
+  DumpText(output->root, extracted_text);
+  std::cout << extracted_text.str() << std::endl;
+  gumbo_destroy_output(&kGumboDefaultOptions, output);
+}
+
+int main(int argc, char** argv) {
+  const char* magic_number = "df6fa1abb58549287111ba8d776733e9";
+  std::ostringstream buffer;
+  string line;
+  string header;
+  while (getline(cin, line)) {
+    if (line.find(magic_number) == 0) {
+      ProcessBuffer(header, buffer.str());
+      buffer.clear();
+      buffer.str(string(""));
+      header = line;
+    } else {
+      buffer << line << std::endl;
+    }
+  }
+  ProcessBuffer(header, buffer.str());
+}
