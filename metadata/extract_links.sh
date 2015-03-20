ce9b9f5da58fb07961f4684f18db02e86aba12ab
Christian Buck <cbuck@lantis.de>
2015-03-20 22:56:44 +0000

    added multithreading and local tmp files

--- metadata/extract_links.sh
+++ metadata/extract_links.sh
@@ -3,10 +3,15 @@
 # Downloads $1 which should be a CommonCrawl wat file,
 # extracts links, sorts by domainname and xzips the result
 
+# Exit as soon as any command fails
+set -e
+
 FILENAME=`echo $1 | awk  ' BEGIN { FS = "/" } { print $(NF-2) "/" $(NF) }'`
 OUTFILE=${FILENAME/warc.wat.gz/links.xz}
+TMPDIR=./tmp/`hostname`
+mkdir -p ${TMPDIR}
 
 # Directory in which this script is stored
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-curl -s $1 | gzip -cd | ${DIR}/links_from_wat.py | sort -t" " -k1,1 | xz > ${OUTFILE}
+curl -s --retry 5 $1 | gzip -cd | ${DIR}/links_from_wat.py | sort -t" " -k1,1 --compress-program=pigz --temporary-directory=${TMPDIR} --parallel=4 | uniq | /home/buck/net/build/pxz/pxz -T 8 > ${OUTFILE} && touch ${OUTFILE/links.xz/done}
