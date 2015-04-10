7d50aee319f7dae441bc8bb69756512b40574e96
Christian Buck <cbuck@lantis.de>
2015-04-10 16:58:57 +0100

    updated DB query tool

--- metadata/query_md.py
+++ metadata/query_md.py
@@ -4,6 +4,7 @@ import sys
 import json
 import urlparse
 import tldextract
+import time
 from collections import defaultdict
 
 
@@ -18,15 +19,16 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('db', help='leveldb root directory')
-    parser.add_argument('--url', help='url to search for',
-                        default='')
-    parser.add_argument('crawl', help='crawl id, e.g. 2013_11')
+    parser.add_argument('url', help='url to search for')
+    # parser.add_argument('crawl', help='crawl id, e.g. 2013_11')
     args = parser.parse_args(sys.argv[1:])
 
     import leveldb
     db = leveldb.LevelDB(args.db)
 
+    start_time = time.time()
     query_tld = get_tld(args.url)
+    # sys.stderr.write("Looking for TLD: %s\n" % query_tld)
     uri2crawl = defaultdict(list)
     crawl2uri = defaultdict(set)
     for key, value in db.RangeIter("%s " % query_tld):
@@ -35,12 +37,15 @@ if __name__ == "__main__":
             break
         data = json.loads(value)
         uri2crawl[uri].append((crawl, data))
-        crawl2uri[crawl].append(uri)
+        crawl2uri[crawl].add(uri)
 
-    sys.stderr.write("found %d unique URLs in %d crawls\n" % (len(uri2crawl),
+    sys.stderr.write("Found %d unique URLs in %d crawls\n" % (len(uri2crawl),
                                                               len(crawl2uri)))
+    sys.stderr.write("Crawl\t#Urls\n")
     for crawl in crawl2uri:
         sys.stderr.write("%s\t%d\n" % (crawl, len(crawl2uri[crawl])))
 
     for uri in uri2crawl:
-        sys.stdout.write("%s\n" %uri)
+        sys.stdout.write("%s\n" % uri)
+
+    sys.stderr.write("Query took %f seconds.\n" % (time.time() - start_time))
