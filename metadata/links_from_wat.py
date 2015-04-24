601a3dbddbf9a8cca04de4759b2f08d954f3d504
Christian Buck <cbuck@lantis.de>
2015-04-24 05:04:36 +0100

    extract content type from metadata

--- metadata/links_from_wat.py
+++ metadata/links_from_wat.py
@@ -6,7 +6,23 @@ import tldextract
 from urlparse import urlparse
 
 
+valid_paths = ["A", "IMG"]
+
+
+def valid_link(link):
+    if "path" not in link:
+        return False
+    if not link["path"].split("@", 1)[0] in valid_paths:
+        return False
+    return True
+
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-nolinks', action='store_true',
+                        help='skip link extraction')
+    args = parser.parse_args(sys.argv[1:])
+
     for line in sys.stdin:
         if not line.startswith("{"):
             continue
@@ -15,19 +31,21 @@ if __name__ == "__main__":
         except ValueError:
             continue
 
-        uri, links = None, None
+        uri, links, container, content_type = None, None, None, None
         try:
             container = d["Container"]
+            content_type = d["Envelope"]["Payload-Metadata"][
+                "HTTP-Response-Metadata"]["Headers"][
+                "Content-Type"]
             uri = d["Envelope"]["WARC-Header-Metadata"]["WARC-Target-URI"]
             links = d["Envelope"]["Payload-Metadata"][
                 "HTTP-Response-Metadata"]["HTML-Metadata"]["Links"]
         except KeyError:
             continue
 
-        if not links:
-            continue
-
-        res = {"uri": uri, "container": container, "links": links}
+        res = {"container": container, "uri": uri, "type": content_type}
+        if not args.nolinks:
+            res["links"] = links
         try:
             tld = tldextract.extract(urlparse(uri).netloc)
         except UnicodeError:
