b75cb7d0dbd75232a81c4fe40067d43aaa4b94ae
Christian Buck <cbuck@lantis.de>
2015-05-06 08:48:40 +0100

    >15x speedup inserting values! \o/

--- metadata/leveldb/insertkv.cc
+++ metadata/leveldb/insertkv.cc
@@ -2,7 +2,9 @@
 
 #include <iostream>
 #include <string>
+#include "leveldb/cache.h"
 #include "leveldb/db.h"
+#include "leveldb/write_batch.h"
 
 using std::string;
 
@@ -15,6 +17,8 @@ int main(int argc, char** argv) {
 
     leveldb::DB* db;
     leveldb::Options options;
+    options.block_cache = leveldb::NewLRUCache(1014 * 1024 * 1024);  // 1GB
+    options.write_buffer_size = 1014 * 1024 * 1024;
     options.create_if_missing = true;
     leveldb::Status status = leveldb::DB::Open(options, argv[1], &db);
     if (!status.ok()) {
@@ -26,15 +30,31 @@ int main(int argc, char** argv) {
 
     string line;
     int nLines = 0;
+    leveldb::WriteBatch batch;
+
     while(std::getline(std::cin, line)) {
         ++nLines;
         const size_t key_end = line.find("\t");
         assert(key_end != std::string::npos);
         const string key  = line.substr(0, key_end);
         const string value = line.substr(key_end+1);
-        db->Put(writeOptions, key, value);
+        batch.Put(key, value);
+
+        if (nLines % 10000 == 0) {
+            status = db->Write(writeOptions, &batch);
+            if (!status.ok()) {
+                std::cerr << "Write error: " << status.ToString() << std::endl;
+            }
+            batch.Clear();
+        }
+    }
+    // Write remaining entries
+    status = db->Write(writeOptions, &batch);
+    if (!status.ok()) {
+        std::cerr << "Write error: " << status.ToString() << std::endl;
     }
 
     delete db;
+    delete options.block_cache;
     return 0;
 }
