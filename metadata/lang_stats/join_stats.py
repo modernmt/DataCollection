072c66040b91556d0e35fcb86221a9ecdadbb3a0
Christian Buck <cbuck@lantis.de>
2015-05-26 14:09:25 -0400

    even negative counts?

--- metadata/lang_stats/join_stats.py
+++ metadata/lang_stats/join_stats.py
@@ -10,7 +10,7 @@ def entropy(lang_dist):
     total = float(sum(lang_dist.values()))
     h = 0
 
-    if total == 0:
+    if total <= 0:
         sys.stderr.write("weird values: total: %f\n" % total)
         return h
 
@@ -21,6 +21,7 @@ def entropy(lang_dist):
         except ValueError:
             sys.stderr.write("weird values: cnt: %d, total: %f\n"
                              % (count, total))
+            return 0
     return h
 
 
@@ -47,7 +48,7 @@ if __name__ == "__main__":
                 continue
 
             num_bytes = int(num_bytes)
-            stats[domain][language] += num_bytes
+            stats[domain][language.upper()] += num_bytes
 
     for domain in stats:
         e = entropy(stats[domain])
