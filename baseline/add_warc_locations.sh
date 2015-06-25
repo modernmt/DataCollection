71d635d433ca25949e2fdc65d23c1cac6aa01877
Achim Ruopp <achim@taus.net>
2015-06-25 08:04:04 +0000

    Updated to current add_warc_locations.py

--- baseline/add_warc_locations.sh
+++ baseline/add_warc_locations.sh
@@ -13,6 +13,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PREFIX=$(dirname ${1}/x | awk -F '/' '{print $NF}')
 
 zcat ${1}/*.links.gz | \
-pypy ${DIR}/add_warc_locations.py --prefix=${PREFIX}/ <(zcat /fs/gna0/buck/cc/warc2stats/baseline_candidates.gz) | \
-gzip -9 > ${1}/baseline_locations.gz
-touch ${1}/baseline_locations.done
+python ${DIR}/add_warc_locations.py --prefix=${PREFIX}/ /home/achim/stats/found_urls.txt \
+> ${1}/found_locations.txt
+touch ${1}/found_locations.done
