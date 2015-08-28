c2d9cbc95b1cb8e7430627c3e443b1a5301c5e50
Christian Buck <cbuck@lantis.de>
2015-08-28 17:00:51 +0100

    use refactored html2text

--- baseline/candidates2bitextor.py
+++ baseline/candidates2bitextor.py
@@ -2,60 +2,9 @@
 # -*- coding: utf-8 -*-
 import sys
 import base64
-import html5lib
-import re
-from html5lib import treebuilders, treewalkers
-
+from html2text import html2text
 
 magic_numer = "df6fa1abb58549287111ba8d776733e9"
-
-# from: https://developer.mozilla.org/en-US/docs/Web/HTML/Block-level_elements
-block_level_elements = set([u'address', u'article', u'aside', u'audio',
-                            u'blockquote', u'canvas', u'dd', u'div', u'dl',
-                            u'fieldset', u'figcaption', u'figure', u'footer',
-                            u'form', u'h1', u'h2', u'h3', u'h4', u'h5', u'h6',
-                            u'header', u'hgroup', u'hr', u'noscript', u'ol',
-                            u'output', u'p', u'pre', u'section', u'table',
-                            u'tfoot', u'ul', u'video'])
-
-
-def clean_whitespace(s):
-    # remove empty lines
-    s = [l.strip() for l in s.split("\n") if l.strip()]
-    return "\n".join(re.sub("\s+", " ", l) for l in s)
-
-
-def hmtl2text(html):
-    p = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
-    dom_tree = p.parse(html.decode("utf-8"))
-    walker = treewalkers.getTreeWalker("dom")
-    stream = walker(dom_tree)
-
-    in_script = False
-    outbuf = []
-    current_line = []
-    for token in stream:
-        token_name = token.get('name', "").lower()
-
-        if token_name in ['script', 'style', 'noscript']:
-            in_script = token.get('type', None) == 'StartTag'
-        if in_script:
-            continue
-
-        if token_name in block_level_elements or token_name == "br":
-            if current_line:
-                outbuf.append(u"".join(current_line))
-                current_line = []
-
-        if token.get(u'type', None) == u'Characters':
-            current_line.append(token['data'])
-        if token.get(u'type', None) == u'SpaceCharacters':
-            if current_line and current_line[-1] != u" ":
-                current_line.append(u" ")
-
-    if current_line:
-        outbuf.append(u"".join(current_line))
-    return clean_whitespace("\n".join(outbuf))
 
 
 def process_buffer(buf, d):
@@ -87,7 +36,7 @@ def read_file(f, d):
 def process_dict(d):
     for u, (header, html) in d.iteritems():
         original_url = header.split()[2]
-        text = hmtl2text(html)
+        text = hmtl2text(html, sanitize=True)
         # html = base64.b64encode(html.encode("utf-8"))
         yield u, original_url, html, text
 
