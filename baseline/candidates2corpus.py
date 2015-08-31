ad313d481e4437d3316bdf47c0b7864f993b529b
Christian Buck <cbuck@lantis.de>
2015-08-31 13:51:46 +0100

    write full HTML to candidates

--- baseline/candidates2corpus.py
+++ baseline/candidates2corpus.py
@@ -12,13 +12,15 @@ from textsanitzer import TextSanitizer
 def process_candidates(candidates, outfile):
     if candidates[0][-1] == "" or candidates[1][-1] == "":
         return
-    src_url, src_text = candidates[0]
-    tgt_url, tgt_text = candidates[1]
+    src_url, src_text, src_html = candidates[0]
+    tgt_url, tgt_text, tgt_html = candidates[1]
 
     outfile.write("\t".join((src_url,
                              tgt_url,
                              base64.b64encode(src_text.encode('utf-8')),
-                             base64.b64encode(tgt_text.encode('utf-8')))))
+                             base64.b64encode(tgt_text.encode('utf-8')),
+                             base64.b64encode(src_html.encode('utf-8')),
+                             base64.b64encode(tgt_html.encode('utf-8')),)))
     outfile.write("\n")
 
 if __name__ == "__main__":
@@ -38,15 +40,17 @@ if __name__ == "__main__":
                 sys.stderr.write("[%d]\n" % linenr)
         url, _crawl, data = line.split('\t', 2)
         data = json.loads(data)
+        # Workaround server error
         if 'offset:' in data:
             data['offset'] = data.pop('offset:')
+        
         html = downloader.download(data['filename'],
                                    int(data[u'offset']),
                                    int(data['length']),
                                    html_only=True)
         html = TextSanitizer.to_unicode(html)
         text = html2text(html.encode('utf-8'), sanitize=True)
-        candidates.append((url, text))
+        candidates.append((url, text, html))
 
         if len(candidates) == 2:
             process_candidates(candidates, args.outfile)
