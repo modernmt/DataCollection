5fffcd50465dc182f128f1ba8abf7d6606edcc82
Christian Buck <cbuck@lantis.de>
2015-11-11 17:29:31 +0000

    added output dir

--- crawlertest/httrack.sh
+++ crawlertest/httrack.sh
@@ -9,8 +9,9 @@ httrack \
 --continue \
 --robots=0 \
 --urlhack \
+--index=0 \
 -m \
 -F 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36' \
 -#L500000000 \
 --skeleton \
-$1
+$1 $2
