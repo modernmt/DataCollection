976d5bceb4d5145ac9782c9a16ab1abdc697adb3
Christrian Buck <cbuck@lantis.de>
2015-11-13 19:28:49 +0100

    added usage

--- crawlertest/httrack.sh
+++ crawlertest/httrack.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+if [ $# != 3 ]; then
+    echo "Usage: $0 start-url output-directory"
+    echo "* start-url: initial seed url"
+    echo "* output-directory: write logs and downloads to this dir"
+    exit
+fi
+
 httrack \
 --connection-per-second=20 \
 --sockets=10 \
