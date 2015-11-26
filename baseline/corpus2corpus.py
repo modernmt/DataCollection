651fcaf4cd3e084b4b9ea1bcdd2984696aa84df8
Christrian Buck <cbuck@lantis.de>
2015-11-26 17:01:27 +0100

    formatting

--- baseline/corpus2corpus.py
+++ baseline/corpus2corpus.py
@@ -34,18 +34,20 @@ if __name__ == "__main__":
     target_text_processor = TextProcessor(splitter=args.target_splitter,
                                           tokenizer=args.target_tokenizer)
 
-for line in sys.stdin:
-    src_url, tgt_url, _, _, src_html, tgt_html = line.strip().split("\t")
+    for line in sys.stdin:
+        src_url, tgt_url, _, _, src_html, tgt_html = line.strip().split("\t")
 
-    src_text = html2text(base64.b64decode(src_html), sanitize=True)
-    tgt_text = html2text(base64.b64decode(tgt_html), sanitize=True)
-    src_text = source_text_processor.process(unicode(src_text))
-    tgt_text = source_text_processor.process(unicode(tgt_text))
+        src_text = html2text(base64.b64decode(src_html), sanitize=True)
+        tgt_text = html2text(base64.b64decode(tgt_html), sanitize=True)
+        src_text = source_text_processor.process(unicode(src_text))
+        tgt_text = source_text_processor.process(unicode(tgt_text))
 
-    args.outfile.write("\t".join((src_url,
-                                  tgt_url,
-                                  base64.b64encode(src_text.encode('utf-8')),
-                                  base64.b64encode(tgt_text.encode('utf-8')),
-                                  src_html,
-                                  tgt_html)))
-    args.outfile.write("\n")
+        args.outfile.write("\t".join((src_url,
+                                      tgt_url,
+                                      base64.b64encode(
+                                          src_text.encode('utf-8')),
+                                      base64.b64encode(
+                                          tgt_text.encode('utf-8')),
+                                      src_html,
+                                      tgt_html)))
+        args.outfile.write("\n")
