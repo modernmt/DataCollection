ece43152db686a377205a3528e4a0ee5eaa9eb94
Christrian Buck <cbuck@lantis.de>
2015-11-26 12:25:02 +0100

    nice output

--- baseline/bitextor_util/show_bitextor_docs.py
+++ baseline/bitextor_util/show_bitextor_docs.py
@@ -6,11 +6,26 @@ import base64
 """ Makes bitextors .docs files humand readable """
 
 for line in sys.stdin:
-    s_url, t_url, source, target = line.split("\t")
-    source = base64.b64decode(source).decode("utf-8")
-    target = base64.b64decode(target).decode("utf-8")
-    sys.stdout.write("\t".join([s_url,
-                                t_url,
-                                repr(source.encode("utf-8")),
-                                repr(target.encode("utf-8"))]))
-    sys.stdout.write("\n")
+    line = line.strip().split("\t")
+    s_url, t_url, source, target = line[:4]
+
+    src_html, tgt_html = '', ''
+
+    # for reading .down files
+    if len(line) == 6:
+        src_html = base64.b64decode(line[4])
+        # print line[5]
+        tgt_html = base64.b64decode(line[5])
+
+    source = base64.b64decode(source)
+    target = base64.b64decode(target)
+
+    sys.stdout.write("----------------")
+    sys.stdout.write("Source URL: %s\n" % s_url)
+    sys.stdout.write("Target URL: %s\n" % s_url)
+    sys.stdout.write("---SOURCE TEXT ---\n\n%s\n" % source)
+    if src_html:
+        sys.stdout.write("---SOURCE HTML ---\n\n%s\n" % src_html)
+    sys.stdout.write("---TARGET TEXT ---\n\n%s\n" % target)
+    if tgt_html:
+        sys.stdout.write("---TARGET HTML ---\n\n%s\n" % tgt_html)
