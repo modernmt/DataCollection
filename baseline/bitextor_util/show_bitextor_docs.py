c606354f10b1b64a437bf7515ba96b5fdd37bc62
Christrian Buck <cbuck@lantis.de>
2015-12-02 01:26:34 +0100

    HTML now optional

--- baseline/bitextor_util/show_bitextor_docs.py
+++ baseline/bitextor_util/show_bitextor_docs.py
@@ -5,27 +5,33 @@ import base64
 
 """ Makes bitextors .docs files humand readable """
 
-for line in sys.stdin:
-    line = line.strip().split("\t")
-    s_url, t_url, source, target = line[:4]
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-html', help='show HTML', action='store_true')
+    args = parser.parse_args()
 
-    src_html, tgt_html = '', ''
+    for line in sys.stdin:
+        line = line.strip().split("\t")
+        s_url, t_url, source, target = line[:4]
 
-    # for reading .down files
-    if len(line) == 6:
-        src_html = base64.b64decode(line[4])
-        # print line[5]
-        tgt_html = base64.b64decode(line[5])
+        src_html, tgt_html = '', ''
 
-    source = base64.b64decode(source)
-    target = base64.b64decode(target)
+        # for reading .down files
+        if len(line) == 6:
+            src_html = base64.b64decode(line[4])
+            # print line[5]
+            tgt_html = base64.b64decode(line[5])
 
-    sys.stdout.write("----------------")
-    sys.stdout.write("Source URL: %s\n" % s_url)
-    sys.stdout.write("Target URL: %s\n" % s_url)
-    sys.stdout.write("---SOURCE TEXT ---\n\n%s\n" % source)
-    if src_html:
-        sys.stdout.write("---SOURCE HTML ---\n\n%s\n" % src_html)
-    sys.stdout.write("---TARGET TEXT ---\n\n%s\n" % target)
-    if tgt_html:
-        sys.stdout.write("---TARGET HTML ---\n\n%s\n" % tgt_html)
+        source = base64.b64decode(source)
+        target = base64.b64decode(target)
+
+        sys.stdout.write("----------------")
+        sys.stdout.write("Source URL: %s\n" % s_url)
+        sys.stdout.write("Target URL: %s\n" % s_url)
+        sys.stdout.write("---SOURCE TEXT ---\n\n%s\n" % source)
+        if src_html and args.html:
+            sys.stdout.write("---SOURCE HTML ---\n\n%s\n" % src_html)
+        sys.stdout.write("---TARGET TEXT ---\n\n%s\n" % target)
+        if tgt_html and args.html:
+            sys.stdout.write("---TARGET HTML ---\n\n%s\n" % tgt_html)
