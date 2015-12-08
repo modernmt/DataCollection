39ffa40efc9a4443e5973806a7c1d163a2375c62
Christrian Buck <cbuck@lantis.de>
2015-12-08 20:32:54 +0100

    added smart encoding prediction

--- baseline/dumptar.py
+++ baseline/dumptar.py
@@ -8,10 +8,11 @@ processed by bitextor pipeline
 
 import sys
 import tarfile
-from util import encoding
+from textsanitzer import TextSanitizer
 
 
 magic_number = "df6fa1abb58549287111ba8d776733e9"
+
 
 if __name__ == "__main__":
     import argparse
@@ -19,6 +20,10 @@ if __name__ == "__main__":
     parser.add_argument('tarfile', help='tarfile containing a webdir')
     parser.add_argument('outfile', type=argparse.FileType('w'),
                         help='output file')
+    parser.add_argument('-unicode', action='store_true',
+                        help='ensure unicode output')
+    parser.add_argument('-language',
+                        help='possible language for encoding guessing')
     args = parser.parse_args(sys.argv[1:])
 
     tar = tarfile.open(args.tarfile, "r:gz")
@@ -27,8 +32,12 @@ if __name__ == "__main__":
         if not tarinfo.isreg():
             continue
         data = tar.extractfile(tarinfo).read()
-        data = encoding.to_unicode(data, is_html=True)
         args.outfile.write("%s uri:%s\n" % (magic_number, tarinfo.name))
-        args.outfile.write(data.encode("utf-8"))
+        if args.unicode:
+            data = TextSanitizer.to_unicode(
+                data, is_html=True, lang=args.language)
+            args.outfile.write(data.encode("utf-8"))
+        else:
+            args.outfile.write(data)
         args.outfile.write("\n")
     tar.close()
