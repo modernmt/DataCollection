5eef8b1a40ab3d9a96305751c5f573c788550065
Christrian Buck <cbuck@lantis.de>
2015-12-21 06:50:59 +0100

    matching for wmt task

---
+++ baseline/languagestripper.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib
+import re
+import urlparse
+
+
+class LanguageStripper(object):
+
+    def __init__(self, languages=None, strip_query_variables=False):
+        self._strip_query_variables = []
+        if strip_query_variables:
+            self._strip_query_variables = [
+                'clang', 'language', 'locale', 'selectedLocale']
+        self.code_to_language = {}
+        # These should all be lower-case, matching is case-insensitive
+        for code in ["arabic", "ara", "ar"]:
+            self.code_to_language[code] = "ar"
+        for code in ["bulgarian", "bul", "bg"]:
+            self.code_to_language[code] = "bg"
+        for code in ["czech", "cze", "cz", "cs"]:
+            self.code_to_language[code] = "cs"
+        for code in ["deutsch", "german", "ger", "deu", "de"]:
+            self.code_to_language[code] = "de"
+        for code in ["english", "eng", "en"]:
+            self.code_to_language[code] = "en"
+        for code in ["espanol", "spanish", "spa", "esp", "es"]:
+            self.code_to_language[code] = "es"
+        for code in ["french", "francais", "fran", "fra", "fre", "fr"]:
+            self.code_to_language[code] = "fr"
+        for code in ["chinese", "chi", "zh"]:
+            self.code_to_language[code] = "zh"
+        # new, not in "Dirt-Cheap"-paper
+        for code in ["tedesco", "de-de", "de-ch", "de-at", "de-li", 'de-lu',
+                     'allemand']:
+            self.code_to_language[code] = "de"
+        for code in ["fr-be", "fr-ca", "fr-fr", "fr-lu", "fr-ch", 'f']:
+            self.code_to_language[code] = "fr"
+        for code in ["italian", "italiano", "ital", 'ita', 'it-it', 'it-ch',
+                     'it']:
+            self.code_to_language[code] = "it"
+        for code in ["en-en", "en-us", "en-uk", 'en-ca', 'en-bz', 'en-ab',
+                     'en-in', 'en-ie', 'en-jm', 'en-nz', 'en-ph', 'en-za',
+                     'en-tt', 'gb', 'en-gb', 'inglese', 'englisch', 'us', 'e']:
+            self.code_to_language[code] = "en"
+        for code in ["romanian", "romana", "romlang", 'rom', 'ro-ro', 'ro']:
+            self.code_to_language[code] = "ro"
+        for code in ["soma", "som", "so", 'somal', 'somali', 'so-so',
+                     'af-soomaali', 'soomaali']:
+            self.code_to_language[code] = "so"
+
+        if languages is not None:
+            kv_pairs = [(k, v) for k, v in self.code_to_language.items()
+                        if v in languages]
+            self.code_to_language = dict(kv_pairs)
+
+        for code, lang in self.code_to_language.items():
+            # add de_de from de-de
+            self.code_to_language[code.replace('-', '_')] = lang
+
+        keys = self.code_to_language.keys()
+        keys.sort(key=len, reverse=True)
+        regexp_string = "(?<![a-zA-Z0-9])(?:%s)(?![a-zA-Z0-9])" % (
+            "|".join(keys))
+        self.re_code = re.compile(regexp_string, re.IGNORECASE)
+
+        # remove "-eng" including the hyphen but not -fr from fr-fr
+        keys = [key for key in keys if '-' not in key and '_' not in key]
+        regexp_string = "[-_](?:%s)(?![a-zA-Z0-9])" % (
+            "|".join(keys))
+        self.re_strip = re.compile(regexp_string, re.IGNORECASE)
+
+        self.re_punct_at_start = re.compile(r'^[^a-zA-Z0-9]+')
+        self.re_punct_at_end = re.compile(r'[^a-zA-Z0-9]+$')
+
+    def strip_path(self, path):
+        components = []
+        for c in path.split('/'):
+            stripped = self.re_strip.sub('', c)
+            stripped = self.re_code.sub('', stripped)
+            if stripped:
+                if not self.re_punct_at_start.match(c) and \
+                        self.re_punct_at_start.match(stripped):
+                    stripped = self.re_punct_at_start.sub('', stripped)
+            if stripped:
+                if not self.re_punct_at_end.match(c) and \
+                        self.re_punct_at_end.match(stripped):
+                    stripped = self.re_punct_at_end.sub('', stripped)
+            if stripped:
+                components.append(stripped)
+        return '/'.join(components)
+
+    def strip_query(self, query):
+        result = []
+        for k, v in urlparse.parse_qsl(query, keep_blank_values=True):
+            if k.lower() in self._strip_query_variables:
+                continue
+            stripped_k = self.re_code.sub('', k)
+            if not stripped_k:
+                continue
+            stripped_v = self.re_code.sub('', v)
+            if stripped_v == v or stripped_v:
+                result.append((k, v))
+        return urllib.urlencode(result)
+
+    def stripn(self, uri):
+        return self.re_code.subn('', uri)
+
+    def strip(self, uri):
+        return self.re_code.sub('', uri)
+
+    def match(self, uri):
+        for match in self.re_code.findall(uri):
+            match = match.lower()
+            assert match in self.code_to_language, \
+                "Unknown match: %s\n" % match
+            return self.code_to_language[match]
+        return ""
+
+    def strip_uri(self, uri, expected_language=None,
+                  remove_index=False):
+        """ Returns (stripped_uri, success) """
+        parsed_uri = urlparse.urlparse(uri)
+
+        matched_languages = [self.match(parsed_uri.path),
+                             self.match(parsed_uri.query)]
+
+        if (expected_language is not None) and \
+                (expected_language not in matched_languages):
+            # we removed a bit of the URL but is does not support our
+            # hope to find expected_language, e.g. removed /fr/ when we were
+            # looking for Italian pages.
+            return "", False
+
+        stripped_path = self.strip_path(parsed_uri.path)
+
+        # repair some stripping artifacts
+        stripped_path = re.sub(r'//+', '/', stripped_path)
+        stripped_path = re.sub(r'__+', '_', stripped_path)
+        stripped_path = re.sub(r'/_+', '/', stripped_path)
+        stripped_path = re.sub(r'_/', '/', stripped_path)
+        stripped_path = re.sub(r'--+', '-', stripped_path)
+
+        # remove new trailing /
+        if stripped_path and stripped_path[-1] == '/' \
+                and parsed_uri.path and parsed_uri.path[-1] != '/':
+            stripped_path = stripped_path[:-1]
+
+        # add removed trailing /
+        if not stripped_path.endswith('/') and parsed_uri.path.endswith('/'):
+            stripped_path += '/'
+
+        stripped_query = self.strip_query(parsed_uri.query)
+
+        # remove index files from tail of path if query empty
+        if remove_index and not stripped_query:
+            if stripped_path.split('/')[-1].startswith('index'):
+                stripped_path = '/'.join(stripped_path.split('/')[:-1])
+
+        netloc = parsed_uri.netloc
+        if '@' in netloc:
+            netloc = netloc.split('@')[1]
+        if ':' in netloc:
+            netloc = netloc.split(':')[0]
+        if not netloc:
+            return "", False
+
+        stripped_uri = urlparse.ParseResult(scheme="http",
+                                            netloc=parsed_uri.netloc,
+                                            path=stripped_path,
+                                            params='',
+                                            query=stripped_query,
+                                            fragment='').geturl()
+
+        return stripped_uri, True
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-languages', help='language codes', nargs='+')
+    args = parser.parse_args()
+
+    language_stripper = LanguageStripper(languages=args.languages)
+
+    for line in sys.stdin:
+        stripped = []
+        changed = False
+        for uri in line.strip().split("\t"):
+            stripped_uri, success = language_stripper.strip_uri(uri)
+            if success:
+                stripped.append(stripped_uri)
+                if stripped_uri != uri:
+                    changed = True
+
+        if changed:
+            print line.strip(), "\t".join(stripped)
--- baseline/match_url_pairs.py
+++ baseline/match_url_pairs.py
@@ -76,24 +76,74 @@ def get_netloc(uri):
     return netloc
 
 
-def strip_urls(urls, lang):
+def strip_urls(urls, lang=None):
     stripped = defaultdict(set)
-    language_stripper = LanguageStripper(languages=[lang])
+    if lang is not None:
+        language_stripper = LanguageStripper(languages=[lang])
+        for url in urls:
+            stripped_url, success = language_stripper.strip_uri(
+                url, expected_language=lang)
+            if success:
+                # if stripped_url in stripped:
+                #     print stripped_url, url, stripped[stripped_url]
+                stripped[stripped_url].add(url)
 
-    for url in urls:
-        stripped_url, success = language_stripper.strip_uri(
-            url, expected_language=lang)
-        if success:
-            # if stripped_url in stripped:
-            #     print stripped_url, url, stripped[stripped_url]
-            stripped[stripped_url].add(url)
+    else:
+        language_stripper = LanguageStripper(strip_query_variables=True)
+        for url in urls:
+            stripped_url, success = language_stripper.strip_uri(
+                url)
+            if stripped_url != url:
+                stripped[stripped_url].add(url)
 
+            stripped_url, success = language_stripper.strip_uri(
+                url, remove_index=True)
+            if stripped_url != url:
+                stripped[stripped_url].add(url)
     return stripped
 
 
-def find_matches(source_stripped, target_stripped):
-    matches = {}
+def find_pairs(source_urls, target_urls,
+               source_stripped, target_stripped,
+               devset):
+    pairs = []
+    # stripped source url matches unstripped target url
+    # e.g. mypage.net/index.html?lang=fr <-> mypage.net/index.html
+    for stripped_source_url in set(
+            source_stripped.iterkeys()).intersection(target_urls):
+        tu = stripped_source_url
+        for su in source_stripped[stripped_source_url]:
+            pairs.append((su, tu))
+    sys.stderr.write("Found %s stripped source + unmodified target pairs\n"
+                     % (len(pairs)))
+    sys.stderr.write("%d pairs from devset\n" %
+                     (len(set(devset).intersection(pairs))))
 
+    # stripped target url matches unstripped source url.
+    # e.g. lesite.fr/en/bonsoir <-> lesite.fr/bonsoir
+    for stripped_target_url in set(
+            target_stripped.iterkeys()).intersection(source_urls):
+        su = stripped_target_url
+        for tu in target_stripped[stripped_target_url]:
+            pairs.append((su, tu))
+    sys.stderr.write("Found %s stripped target + unmodified source pairs\n"
+                     % (len(pairs)))
+    sys.stderr.write("%d pairs from devset\n" %
+                     (len(set(devset).intersection(pairs))))
+
+    # stripped source url matches stripped target url
+    # e.g. page.net/fr <-> page.net/en
+    for stripped_source_url, source_url in source_stripped.iteritems():
+        if stripped_source_url in target_stripped:
+            for su in source_url:
+                for tu in target_stripped[stripped_source_url]:
+                    pairs.append((su, tu))
+
+    sys.stderr.write("Found %d pairs\n" % (len(pairs)))
+    sys.stderr.write("%d pairs from devset\n" %
+                     (len(set(devset).intersection(pairs))))
+
+    return pairs
 
 if __name__ == "__main__":
     import argparse
@@ -151,142 +201,161 @@ if __name__ == "__main__":
     print "%d/%d stripped source/target urls" % (len(source_stripped),
                                                  len(target_stripped))
 
-    pairs = []
-    # stripped source url matches unstripped target url
-    # e.g. mypage.net/index.html?lang=fr <-> mypage.net/index.html
-    for stripped_source_url in set(
-            source_stripped.iterkeys()).intersection(target_urls):
-        tu = stripped_source_url
-        for su in source_stripped[stripped_source_url]:
-            pairs.append((su, tu))
-    sys.stderr.write("Found %s stripped source + unmodified target pairs\n"
-                     % (len(pairs)))
+    pairs = find_pairs(source_urls, target_urls,
+                       source_stripped, target_stripped,
+                       devset)
 
-    # stripped target url matches unstripped source url.
-    # e.g. lesite.fr/en/bonsoir <-> lesite.fr/bonsoir
-    for stripped_target_url in set(
-            target_stripped.iterkeys()).intersection(source_urls):
-        su = stripped_target_url
-        for tu in target_stripped[stripped_target_url]:
-            pairs.append((su, tu))
-    sys.stderr.write("Found %s stripped target + unmodified source pairs\n"
-                     % (len(pairs)))
+    source_stripped = strip_urls(source_urls)
+    target_stripped = strip_urls(target_urls)
+    print "%d/%d stripped source/target urls" % (len(source_stripped),
+                                                 len(target_stripped))
 
-    # stripped source url matches stripped target url
-    # e.g. page.net/fr <-> page.net/en
-    for stripped_source_url, source_url in source_stripped.iteritems():
-        if stripped_source_url in target_stripped:
-            for su in source_url:
-                for tu in target_stripped[stripped_source_url]:
-                    pairs.append((su, tu))
+    pairs.extend(find_pairs(source_urls, target_urls,
+                            source_stripped, target_stripped,
+                            devset))
+    print "%d/%d stripped source/target urls" % (len(source_stripped),
+                                                 len(target_stripped))
 
-    sys.stderr.write("Found %s pairs\n" % (len(pairs)))
+    # Filter pairs so that every url only occurs once
+    pairs_s2t, pairs_t2s = {}, {}
+    for su, tu in pairs:
+        if su not in pairs_s2t and tu not in pairs_t2s:
+            pairs_s2t[su] = tu
+            pairs_t2s[tu] = su
+
+            if args.pairs:
+                args.pairs.write("%s\t%s\n" % (su, tu))
+
+    sys.stderr.write("Found %d pairs\n" % (len(pairs_s2t)))
+    sys.stderr.write("%d pairs from devset\n" %
+                     (len(set(devset).intersection(pairs_s2t.items()))))
+
+    # write wins and fails
+    if args.fails:
+        for su, tu in set(devset).difference(pairs_s2t.items()):
+            if su in pairs_s2t:
+                args.fails.write("S2T MISMATCH: %s\t->\t%s\texpected: %s\n" %
+                                 (su, pairs_s2t[su], tu))
+            if tu in pairs_t2s:
+                args.fails.write("T2S MISMATCH: %s\t->\t%s\texpected: %s\n" %
+                                 (tu, pairs_t2s[tu], tu))
+            if su not in pairs_s2t and tu not in pairs_t2s:
+                args.fails.write("Missing: %s\t->\t%s\n" %
+                                 (su, tu))
+
+            if (su, tu) in pairs:
+                args.fails.write("Was in pairs\n")
+
+    if args.wins:
+        for su, tu in set(devset).intersection(pairs_s2t.items()):
+            args.wins.write("%s\t%s\n" % (su, tu))
+
     sys.exit()
 
-    unstripped_urls = set(urls.keys())
+    # unstripped_urls = set(urls.keys())
 
-    for lang in args.languages:
-        language_stripper = LanguageStripper(languages=[lang])
-        # language_stripper = LanguageStripper()
+    # for lang in args.languages:
+    #     language_stripper = LanguageStripper(languages=[lang])
+    # language_stripper = LanguageStripper()
 
-        for url in unstripped_urls:
+    #     for url in unstripped_urls:
 
-            stripped_url, success = language_stripper.strip_uri(url)
-            if success:
-                urls[stripped_url].add(url)
+    #         stripped_url, success = language_stripper.strip_uri(url)
+    #         if success:
+    #             urls[stripped_url].add(url)
 
-            # if stripped_url != url:
-            #     stripped_urls[stripped_url] = url
+    # if stripped_url != url:
+    # stripped_urls[stripped_url] = url
 
-    n_pairs = 0
-    for key, mapped_urls in urls.iteritems():
-        if len(mapped_urls) > 1:
-            n_pairs += 1
+    # n_pairs = 0
+    # for key, mapped_urls in urls.iteritems():
+    #     if len(mapped_urls) > 1:
+    #         n_pairs += 1
 
-    pairs = {}
-    for key, mapped_urls in urls.iteritems():
-        mapped_urls = n_longest(mapped_urls, n=2, discounted=unstripped_urls)
-        if len(mapped_urls) == 2:
-            # print "\t".join(list(mapped_urls))
-            u1, u2 = mapped_urls
-            assert u1 != u2
-            if u1 in pairs and pairs[u1] != u2:
-                sys.stderr.write("%s -> %s in pairs: %s\n" %
-                                 (u1, u2, pairs[u1]))
-            # assert u1 not in pairs
-            if u2 in pairs and pairs[u2] != u1:
-                sys.stderr.write("%s -> %s in pairs: %s\n" %
-                                 (u2, u1, pairs[u2]))
-            # assert u2 not in pairs
-            pairs[u1] = u2
-            pairs[u2] = u1
-            if args.pairs:
-                # if u1 > u2:
-                #     u1, u2 = u2, u1
-                args.pairs.write("%s\t%s\n" % (u1, u2))
-        # elif len(mapped_urls) > 2:
-        # print "Found more than 2 matching urls:", "\t".join(mapped_urls)
-        #     for i, u1 in enumerate(mapped_urls):
-        #         for u2 in mapped_urls[i + 1:]:
-        #             args.pairs.write("%s\t%s\n" % (u1, u2))
+    # pairs = {}
+    # for key, mapped_urls in urls.iteritems():
+    #     mapped_urls = n_longest(mapped_urls, n=2, discounted=unstripped_urls)
+    #     if len(mapped_urls) == 2:
+    # print "\t".join(list(mapped_urls))
+    #         u1, u2 = mapped_urls
+    #         assert u1 != u2
+    #         if u1 in pairs and pairs[u1] != u2:
+    #             sys.stderr.write("%s -> %s in pairs: %s\n" %
+    #                              (u1, u2, pairs[u1]))
+    # assert u1 not in pairs
+    #         if u2 in pairs and pairs[u2] != u1:
+    #             sys.stderr.write("%s -> %s in pairs: %s\n" %
+    #                              (u2, u1, pairs[u2]))
+    # assert u2 not in pairs
+    #         pairs[u1] = u2
+    #         pairs[u2] = u1
+    #         if args.pairs:
+    # if u1 > u2:
+    # u1, u2 = u2, u1
+    #             args.pairs.write("%s\t%s\n" % (u1, u2))
+    # elif len(mapped_urls) > 2:
+    # print "Found more than 2 matching urls:", "\t".join(mapped_urls)
+    # for i, u1 in enumerate(mapped_urls):
+    # for u2 in mapped_urls[i + 1:]:
+    # args.pairs.write("%s\t%s\n" % (u1, u2))
 
-    print "Found %d pairs" % (len(pairs) / 2)
+    # print "Found %d pairs" % (len(pairs) / 2)
     # devset = dict(devset)
 
     # adding close matches for stripped urls:
-    if args.augment:
-        netloc2strippedurls = defaultdict(list)
-        for url in stripped_urls.keys():
-            netloc2strippedurls[get_netloc(url)].append(url)
+    # if args.augment:
+    #     netloc2strippedurls = defaultdict(list)
+    #     for url in stripped_urls.keys():
+    #         netloc2strippedurls[get_netloc(url)].append(url)
 
-        n_stripped_added = 0
-        for stripped_url, url in stripped_urls.iteritems():
-            if url not in pairs:
-                for cand in get_close_matches(stripped_url,
-                                              netloc2strippedurls[
-                                                  get_netloc(url)],
-                                              100, cutoff=0.9):
-                    if stripped_urls[cand] in pairs:
-                        continue
-                    u1 = url
-                    u2 = stripped_urls[cand]
-                    if u1 == u2:
-                        continue
-                    pairs[u1] = u2
-                    pairs[u2] = u1
-                    distance = SequenceMatcher(None, u1, u2).ratio()
-                    print "Adding %f %s <-> %s" % (distance, u1, u2)
-                    n_stripped_added += 1
-                    if args.pairs:
-                        args.pairs.write("%s\t%s\n" % (u1, u2))
-                    break
-        print "Added %d pairs from similar looking stripped urls" % (
-            n_stripped_added)
-        print "Total %d pairs" % (len(pairs) / 2)
+    #     n_stripped_added = 0
+    #     for stripped_url, url in stripped_urls.iteritems():
+    #         if url not in pairs:
+    #             for cand in get_close_matches(stripped_url,
+    #                                           netloc2strippedurls[
+    #                                               get_netloc(url)],
+    #                                           100, cutoff=0.9):
+    #                 if stripped_urls[cand] in pairs:
+    #                     continue
+    #                 u1 = url
+    #                 u2 = stripped_urls[cand]
+    #                 if u1 == u2:
+    #                     continue
+    #                 pairs[u1] = u2
+    #                 pairs[u2] = u1
+    #                 distance = SequenceMatcher(None, u1, u2).ratio()
+    #                 print "Adding %f %s <-> %s" % (distance, u1, u2)
+    #                 n_stripped_added += 1
+    #                 if args.pairs:
+    #                     args.pairs.write("%s\t%s\n" % (u1, u2))
+    #                 break
+    #     print "Added %d pairs from similar looking stripped urls" % (
+    #         n_stripped_added)
+    #     print "Total %d pairs" % (len(pairs) / 2)
 
-    correct = 0
-    no_matching = []
-    mismatch = []
-    if devset:
-        for u1, u2 in devset:
-            if u1 not in pairs:
-                no_matching.append(u1)
-                args.fails.write("NO MATCH: %s [expected: %s]\n" % (u1, u2))
-                if u2 in pairs:
-                    args.fails.write(
-                        "-> U2 %s found with %s [expected %s]\n" % (u2,
-                                                                    pairs[u2],
-                                                                    u1))
-            elif pairs[u1] != u2:
-                mismatch.append((u1, u2, pairs[u1]))
-                if args.fails:
-                    args.fails.write("MISMATCH: %s\t->\t%s\texpected: %s\n" %
-                                     (u1, pairs[u1], u2))
-            else:
-                assert pairs[u1] == u2
-                if args.wins:
-                    args.wins.write("%s\t%s\n" % (u1, u2))
-                correct += 1
+    # correct = 0
+    # no_matching = []
+    # mismatch = []
+    # if devset:
+    #     for u1, u2 in devset:
+    #         if u1 not in pairs:
+    #             no_matching.append(u1)
+    #             args.fails.write("NO MATCH: %s [expected: %s]\n" % (u1, u2))
+    #             if u2 in pairs:
+    #                 args.fails.write(
+    #                     "-> U2 %s found with %s [expected %s]\n" % (u2,
+    #                                                                 pairs[u2],
+    #                                                                 u1))
+    #         elif pairs[u1] != u2:
+    #             mismatch.append((u1, u2, pairs[u1]))
+    #             if args.fails:
+    #                 args.fails.write("MISMATCH: %s\t->\t%s\texpected: %s\n" %
+    #                                  (u1, pairs[u1], u2))
+    #         else:
+    #             assert pairs[u1] == u2
+    #             if args.wins:
+    #                 args.wins.write("%s\t%s\n" % (u1, u2))
+    #             correct += 1
 
-    print "Correct: %d/%d = %f" % (correct, len(devset), float(correct) /
-                                   len(devset))
+    # print "Correct: %d/%d = %f" % (correct, len(devset), float(correct) /
+    #                                len(devset))
