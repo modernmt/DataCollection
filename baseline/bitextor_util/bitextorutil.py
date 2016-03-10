1e1d0a96c829fa943f26a3a30b648d29914db591
Christian Buck <cbuck@lantis.de>
2016-03-10 00:51:51 +0000

    updated for compatibility with new bitextor version

--- baseline/bitextor_util/bitextorutil.py
+++ baseline/bitextor_util/bitextorutil.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-import sys
 import base64
-from collections import defaultdict
-import codecs
+from nltk import wordpunct_tokenize
 
+import sys
 sys.path.append('/home/buck/net/build/bitextor/share/bitextor/utils')
 from unicodepunct import get_unicode_punct
-from nltk import word_tokenize
-
 punctuation_chars = get_unicode_punct()
+
+# same tokenization as used in bitextor-lett2idx
 
 
 def get_words(untokenized_text):
-    text = " ".join(word_tokenize(untokenized_text))
+    text = " ".join(wordpunct_tokenize(untokenized_text))
     words = text.lower().split()
     words = [w.strip(punctuation_chars) for w in words]
     words = [w for w in words if w]
@@ -27,7 +26,9 @@ def read_lett(filename, lang=None, as_set=False):
             fields = line.strip().split("\t")
             if lang is not None and lang != fields[0]:
                 continue
-            text = base64.b64decode(fields[5]).decode('utf-8').strip()
+            text = base64.b64decode(fields[6])
+            # print repr(text)
+            text = text.decode('utf-8')
             if text:
                 words = get_words(text)
                 if as_set:
