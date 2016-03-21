3775633bdd1120c389fe4d6ffa4603ee6d9ec2af
Christian Buck <cbuck@lantis.de>
2016-03-21 13:40:53 +0000

    allow n:m devset, load and save matrices

--- docaligner/eval_model.py
+++ docaligner/eval_model.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import numpy as np
-import sys
-import json
-import pickle
 from datetime import datetime
 from scipy.stats import pearsonr, spearmanr
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
-from sklearn import linear_model
+import gzip
+import json
+import numpy as np
+import pickle
+import sys
+from sklearn import preprocessing
+from collections import defaultdict
 
-sys.path.append("/home/buck/net/build/DataCollection/baseline")
-from strip_language_from_uri import LanguageStripper
+# sys.path.append("/home/buck/net/build/DataCollection/baseline")
+# from strip_language_from_uri import LanguageStripper
 
 
 def read_devset(fh, mapping):
@@ -30,83 +32,56 @@ def read_devset(fh, mapping):
     return devset
 
 
-# def read_devset(fh, mapping):
-# format fr-url <TAB> en-url
-#     devset = set()
-#     url_matched = []
-#     stripper = LanguageStripper()
-#     print "Reading devset from ", fh.name
-#     seen = set()
-#     for line in fh:
-#         turl, surl = line.strip().split()
-#         if turl not in seen and surl not in seen:
-#             if turl not in mapping['target_url_to_index']:
-#                 if "creationwiki.org" in turl:
-#                     sys.stderr.write("Unknown target URL: %s\n" % turl)
-#             elif surl not in mapping['source_url_to_index']:
-#                 if "creationwiki.org" in surl:
-#                     sys.stderr.write("Unknown source URL: %s\n" % surl)
-#             else:
-#                 assert turl in mapping['target_url_to_index']
-#                 assert surl in mapping['source_url_to_index']
-#                 tidx = mapping['target_url_to_index'][turl]
-#                 sidx = mapping['source_url_to_index'][surl]
-#                 devset.add((sidx, tidx))
-#                 seen.add(turl)
-#                 seen.add(surl)
+def read_devset_multi(fh, mapping):
+    # format fr-url <TAB> en-url
+    devset = set()
+    n_pairs = 0
+    print "Reading multi-devset from ", fh.name
+    for line in fh:
+        surls, turls = line.split('\t<->\t')
+        surls = surls.strip().split('\t')
+        turls = turls.strip().split('\t')
 
-# print stripper.strip(surl), stripper.strip(turl)
-#                 if stripper.strip(surl) == stripper.strip(turl):
-#                     url_matched.append((surl, turl))
+        added = False
+        for surl in surls:
+            if surl in mapping['source_url_to_index']:
+                for turl in turls:
+                    assert turl in mapping['target_url_to_index']
+                    if turl in mapping['target_url_to_index']:
+                        assert surl in mapping['source_url_to_index']
+                        tidx = mapping['target_url_to_index'][turl]
+                        sidx = mapping['source_url_to_index'][surl]
+                        devset.add((sidx, tidx))
+                        added = True
+        if added:
+            n_pairs += 1
 
-#         else:
-#             print "already seen:"
-#             print surl, turl
-#             print devset
-#             sys.exit()
+    return devset, n_pairs
 
-#     print "url_matched:", len(url_matched), "/", len(devset)
-# sys.exit()
-#     return devset
+
+def find_pairs_in_devset(matches, devset):
+    s2t = defaultdict(set)
+    t2s = defaultdict(set)
+    for si, ti in devset:
+        s2t[si].add(ti)
+        t2s[ti].add(si)
+
+    seen_s, seen_t = set(), set()
+    found = set()
+
+    for si, ti in matches:
+        if si in seen_s or ti in seen_t:
+            continue
+        if si in s2t and ti in s2t[si]:
+            found.add((si, ti))
+            seen_s.update(t2s[ti])
+            seen_t.update(s2t[si])
+    return found
 
 
 def read_idx2url(fh):
     mapping = json.load(fh)
     return mapping
-
-
-# def cut_features(feature_list, devset, mapping):
-# col = targets
-# rows = sources
-#     cols, rows = [], []
-#     for turl, surl in devset.iteritems():
-#         cols.append(mapping['target_url_to_index'][turl])
-#         rows.append(mapping['source_url_to_index'][surl])
-#     cols.sort()
-#     rows.sort()
-
-# We have 1-1 mapping which gives a square matrix
-#     new_target = np.zeros((len(rows), len(cols)))
-
-#     for turl, surl in devset.iteritems():
-#         sidx = mapping['source_url_to_index'][surl]
-#         sidx = rows.index(sidx)
-#         tidx = mapping['target_url_to_index'][turl]
-#         tidx = cols.index(tidx)
-
-#         new_target[sidx, tidx] = 1
-
-#     new_features = []
-#     for f in features:
-#         print f.shape
-#         print (len(mapping['source_url_to_index']),
-#                len(mapping['target_url_to_index']))
-#         assert f.shape == (len(mapping['source_url_to_index']),
-#                            len(mapping['target_url_to_index']))
-#         nf = f[rows][:, cols]
-#         new_features.append(nf)
-
-#     return new_features, new_target
 
 if __name__ == "__main__":
     import argparse
@@ -118,7 +93,7 @@ if __name__ == "__main__":
     parser.add_argument('-read_model',
                         help='read fitted model',
                         type=argparse.FileType('rb'))
-    parser.add_argument('feature_matrix', nargs='+',
+    parser.add_argument('feature_matrix', nargs='*',
                         help='precomputed matrix for single feature',
                         type=argparse.FileType('r'))
     parser.add_argument('-matching',
@@ -127,116 +102,106 @@ if __name__ == "__main__":
     parser.add_argument('-prefix',
                         help='output prefix (domain name)',
                         default="PREFIX")
-
+    parser.add_argument('-scale',
+                        help='scale input matrices',
+                        action='store_true')
+    parser.add_argument('-multi',
+                        help='n:m devset',
+                        action='store_true')
+    parser.add_argument('-write_matrix',
+                        help='write concatenated feature matrix',
+                        type=argparse.FileType('w'))
+    parser.add_argument('-write_predictions',
+                        help='write predicted values',
+                        type=argparse.FileType('w'))
+    parser.add_argument('-load_predictions',
+                        help='load prediction matrix',
+                        type=argparse.FileType('r'))
     args = parser.parse_args(sys.argv[1:])
 
-    print "Loading model from ", args.read_model.name
-    fitted_model = pickle.load(args.read_model)
-    # print "Coef:", fitted_model.coef_
-
     url_mapping = read_idx2url(args.idx2url)
-    devset = read_devset(args.devset, url_mapping)
-    print "Loaded %d expected pairs from %s" % (len(devset), args.devset.name)
+    devset = None
+    devset_size = 0
+    if args.multi:
+        devset, devset_size = read_devset_multi(args.devset, url_mapping)
+    else:
+        devset = read_devset(args.devset, url_mapping)
+        devset_size = len(devset)
+    print "Loaded %d/%d expected pairs from %s" % (
+        devset_size, len(devset), args.devset.name)
 
-    # print "Loading features from ", [fh.name for fh in args.feature_matrix]
-    # features = map(np.load, args.feature_matrix)
+    score_matrix = None
+    if args.feature_matrix:
+        print "Loading model from ", args.read_model.name
+        fitted_model = pickle.load(args.read_model)
+        # print "Coef:", fitted_model.coef_
 
-    # n_source, n_target = features[0].shape
+        n_features = len(args.feature_matrix)
+        n_samples, n_source, n_target = None, None, None
+        m = None
 
-    n_features = len(args.feature_matrix)
-    n_samples, n_source, n_target = None, None, None
-    m = None
+        for f_idx, f_file in enumerate(args.feature_matrix):
+            print "Loading features from ", f_file.name
+            fh = f_file
+            if f_file.name.endswith('.gz'):
+                fh = gzip.GzipFile(fileobj=fh, mode='r')
+            f = np.load(fh)
+            if args.scale:
+                print "scaling"
+                f = preprocessing.scale(f)
+            # f = f.astype(np.float32, copy=False)
 
-    for f_idx, f_file in enumerate(args.feature_matrix):
-        print "Loading features from ", f_file.name
-        f = np.load(f_file)
-        if m is None:
-            n_samples = f.size
-            n_source, n_target = f.shape
-            m = np.zeros((n_samples, n_features))
-        else:
-            assert f.shape == (n_source, n_target)
-        m[:, f_idx] = f.flatten()
+            if m is None:
+                n_samples = f.size
+                n_source, n_target = f.shape
+                m = np.zeros((n_samples, n_features), dtype=np.float64)
+            else:
+                assert f.shape == (n_source, n_target)
+            m[:, f_idx] = f.flatten()
 
-    print "%d source / %d target docs / %d features" \
-        % (n_source, n_target, n_features)
+        print datetime.now()
 
-    # TODO: make this more elegant
-    # for s_idx in range(n_source):
-    #     for t_idx in range(n_target):
+        print "%d source / %d target docs / %d features" \
+            % (n_source, n_target, n_features)
 
-    #         sample_idx = s_idx * n_target + t_idx
+        if args.write_matrix:
+            np.save(args.write_matrix, m)
 
-    #         for f_idx in range(n_features):
-    #             m[sample_idx, f_idx] = features[f_idx][s_idx, t_idx]
+        print "Predicting %d instances." % (m.shape[0])
+        predicted = None
+        if hasattr(fitted_model, "predict_proba"):
+            predicted = fitted_model.predict_proba(m)[:, 1]
+        else:  # use decision function
+            print "Using predict instead of predict_proba"
+            prob_pos = fitted_model.predict(m)
+            predicted = (prob_pos - prob_pos.min()) / \
+                (prob_pos.max() - prob_pos.min())
 
-    print "Predicting %d instances." % (m.shape[0])
-    predicted = fitted_model.predict_proba(m)
-    predicted = predicted[:, 1]  # we're interested in probs for class 1
+        # predicted = fitted_model.predict_proba(m)
+        # predicted = predicted[:, 1]  # we're interested in probs for class 1
+        del m
 
-    score_matrix = predicted.reshape((n_source, n_target))
+        print predicted
+        score_matrix = predicted.reshape((n_source, n_target))
 
-    # print datetime.now()
-    # print "Finding best match (greedy)"
-    # greedy_matches = set()
-    # correct, errors = [], []
-    # for s_idx in range(n_source):
-    #     t_idx = np.argmax(score_matrix[s_idx])
-    #     greedy_matches.add((s_idx, t_idx))
+        if args.write_predictions:
+            np.save(args.write_predictions, score_matrix)
+            # evaluate later
+            sys.exit()
 
-    # found = devset.intersection(greedy_matches)
-    # print "Found %d matches " % (len(greedy_matches))
-    # print "Found %d out of %d pairs = %f%%" \
-    #     % (len(found), len(devset), 100. * len(found) / len(devset))
-
-    # print datetime.now()
-    # print "Finding best match (greedy / restricted)"
-    # matches = set()
-    # correct, errors = [], []
-    # score_copy = score_matrix.copy()
-    # while True:
-    #     am = np.argmax(score_copy)
-    #     am_row, am_col = np.unravel_index(am, score_copy.shape)
-    #     if score_copy[am_row, am_col] <= 0:
-    #         break
-    #     matches.add((am_row, am_col))
-    #     score_copy[am_row, :] = -np.inf
-    #     score_copy[:, am_col] = -np.inf
-    # del score_copy
-    # print "Found %d matches " % (len(matches))
-    # found = devset.intersection(matches)
-    # print "Found %d out of %d pairs = %f%%" \
-    #     % (len(found), len(devset), 100. * len(found) / len(devset))
-
-    print datetime.now()
-    print "Finding best match (greedy / restricted + argsort)"
-    matches = set()
-    seen_cols = set()
-    seen_rows = set()
-    sorted_indices = np.argsort(score_matrix, axis=None)
-    for idx in sorted_indices[::-1]:
-        am_row, am_col = np.unravel_index(idx, score_matrix.shape)
-        if am_row in seen_rows or am_col in seen_cols:
-            continue
-        matches.add((am_row, am_col))
-        seen_cols.add(am_col)
-        seen_rows.add(am_row)
-
-    print "Found %d matches " % (len(matches))
-    # print greedy_matches
-    found = devset.intersection(matches)
-    print "Found %d out of %d pairs = %f%%" \
-        % (len(found), len(devset), 100. * len(found) / len(devset))
-
-    print "RES:\t%s\t%s\t%d\t%d" % (args.prefix, args.read_model.name,
-                                    len(found), len(devset))
-
-    #
+    else:
+        assert args.load_predictions
+        fh = args.load_predictions
+        if fh.name.endswith('.gz'):
+            fh = gzip.GzipFile(fileobj=fh, mode='r')
+        score_matrix = np.load(fh)
+        print "Loaded matrix of shape", score_matrix.shape, \
+              " from ", args.load_predictions.name
 
     print datetime.now()
     if args.matching:
         print "Finding best matching"
-        matching_pairs = set()
+        matches = set()
 
         full_matrix = np.pad(
             score_matrix,
@@ -256,14 +221,37 @@ if __name__ == "__main__":
         for sidx, tidx in enumerate(assignment):
             if sidx >= score_matrix.shape[0] or tidx >= score_matrix.shape[1]:
                 continue
-            matching_pairs.add((sidx, tidx))
+            matches.add((sidx, tidx))
 
-        print "Found %d matches " % (len(matching_pairs))
-        # print greedy_matches
-        found = devset.intersection(matching_pairs)
-        print "Found %d out of %d pairs = %f%%" \
-            % (len(found), len(devset), 100. * len(found) / len(devset))
-        print datetime.now()
+    else:
+        print "Finding best match (greedy / restricted + argsort)"
+        matches = set()
+        seen_cols = set()
+        seen_rows = set()
+        sorted_indices = np.argsort(score_matrix, axis=None, kind='mergesort')
+        for idx in sorted_indices[::-1]:
+            am_row, am_col = np.unravel_index(idx, score_matrix.shape)
+            if am_row in seen_rows or am_col in seen_cols:
+                continue
+            matches.add((am_row, am_col))
+            seen_cols.add(am_col)
+            seen_rows.add(am_row)
+
+    print "Found %d matches " % (len(matches))
+    found = find_pairs_in_devset(matches, devset)
+    print "Found %d out of %d pairs = %f%%" \
+        % (len(found), len(devset), 100. * len(found) / devset_size)
+
+    name = None
+    if args.read_model:
+        name = args.read_model.name
+    else:
+        assert args.load_predictions
+        name = args.load_predictions.name
+    print "RES:\t%s\t%s\t%d\t%d" % (args.prefix, name,
+                                    len(found), devset_size)
+
+    print datetime.now()
 
     sys.exit()
 
